77b3604950180cf08f839889f94843a716a83975
danchouzhou <50841206+danchouzhou@users.noreply.github.com>
2019-05-28 17:48:01 -0400

    Add files via upload

---
+++ firmware/SampleCode/m453_canbus_tx/Debug/User/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../User/main.c 
+
+OBJS += \
+./User/main.o 
+
+C_DEPS += \
+./User/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+User/%.o: ../User/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM GNU C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -I"../Library/CMSIS/Include" -I"../Library/Device/Nuvoton/M451Series/Include" -I"../Library/StdDriver/inc" -I"../Library/CMSIS/Include" -I"../Library/Device/Nuvoton/M451Series/Include" -I"../Library/StdDriver/inc" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
