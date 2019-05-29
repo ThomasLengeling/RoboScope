d7461e120561c383539ba1a14998f0ef01e41df3
ThomasLengeling <aa.no.tom@gmail.com>
2019-05-29 14:40:06 -0400

    organize folders

---
+++ Code/CamExample/m453_usbserial/Debug/CMSIS/GCC/subdir.mk
@@ -0,0 +1,38 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../CMSIS/GCC/_syscalls.c 
+
+S_UPPER_SRCS += \
+../CMSIS/GCC/startup_M451Series.S 
+
+OBJS += \
+./CMSIS/GCC/_syscalls.o \
+./CMSIS/GCC/startup_M451Series.o 
+
+S_UPPER_DEPS += \
+./CMSIS/GCC/startup_M451Series.d 
+
+C_DEPS += \
+./CMSIS/GCC/_syscalls.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+CMSIS/GCC/%.o: ../CMSIS/GCC/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM GNU C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -I"../Library/CMSIS/Include" -I"../Library/Device/Nuvoton/M451Series/Include" -I"../Library/StdDriver/inc" -I"../Library/CMSIS/Include" -I"../Library/Device/Nuvoton/M451Series/Include" -I"../Library/StdDriver/inc" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+CMSIS/GCC/%.o: ../CMSIS/GCC/%.S
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM GNU Assembler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -x assembler-with-cpp -I"../Library/CMSIS/Include" -I"../Library/Device/Nuvoton/M451Series/Include" -I"../Library/StdDriver/inc" -I"../Library/CMSIS/Include" -I"../Library/Device/Nuvoton/M451Series/Include" -I"../Library/StdDriver/inc" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Code/CamExample/m453_usbserial/Debug/User/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../User/cdc_serial.c \
+../User/descriptors.c \
+../User/main.c 
+
+OBJS += \
+./User/cdc_serial.o \
+./User/descriptors.o \
+./User/main.o 
+
+C_DEPS += \
+./User/cdc_serial.d \
+./User/descriptors.d \
+./User/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+User/%.o: ../User/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM GNU C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -I"../Library/CMSIS/Include" -I"../Library/Device/Nuvoton/M451Series/Include" -I"../Library/StdDriver/inc" -I"../Library/CMSIS/Include" -I"../Library/Device/Nuvoton/M451Series/Include" -I"../Library/StdDriver/inc" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
